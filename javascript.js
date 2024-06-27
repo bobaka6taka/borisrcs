950818299d88c55698462c679df84a624fd770c7
GreeningSiren <greeningsiren@gmail.com>
2024-06-27 07:18:16 +0000

    refactor:💡 using localStorage instead of cookies //test

--- javascript.js
+++ javascript.js
@@ -10,26 +10,8 @@ let docTitle = document.title;
 const slider = document.querySelector('.theme input');
 const root = document.querySelector(':root');
 
-function setCookie(name, value, days) {
-  const date = new Date();
-  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/borisrcs/";
-}
-
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
 function initializeSlider() {
-  const savedColor = getCookie('sliderColor');
+  const savedColor = localStorage.getItem('sliderColor');;
   if (savedColor) {
     document.getElementById('colorSlider').value = savedColor;
     let hue = savedColor;
@@ -48,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 slider.addEventListener('input', () => {
   let hue = slider.value;
-  setCookie('sliderColor', hue, 365); // save for one year
+  localStorage.setItem("sliderColor", hue); // save forever
   root.style.setProperty('--primary-color', `oklch(45.12% 0.267 ${hue})`);
   root.style.setProperty('--secondary-color', `oklch(94.45% 0.03 ${hue})`);
 });
